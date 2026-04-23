ad3e14297554e7d8a2eabbd61ae8cdc9376fcded
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-04-23 12:05:56 +0300

    feat: All base logics for BookingApplicationService with @Transactional and exceptions feat: add findByUserId method to BookingRepository

--- backend/booking-service/src/main/java/org/example/repository/BookingRepository.java
+++ backend/booking-service/src/main/java/org/example/repository/BookingRepository.java
@@ -3,5 +3,8 @@ package org.example.repository;
 import org.example.domain.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+  public List<Booking> findByUserId(Long userId);
 }
