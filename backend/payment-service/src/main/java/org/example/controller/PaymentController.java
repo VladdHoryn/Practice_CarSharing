36a730a6a7ad824d072bb705440da962d508070a
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-05-12 12:11:21 +0300

    feat: create PaymentRepository; PaymentApplicationService; PaymentController

---
+++ backend/payment-service/src/main/java/org/example/controller/PaymentController.java
@@ -0,0 +1,10 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("paymetns/v1")
+public class PaymentController {
+}
