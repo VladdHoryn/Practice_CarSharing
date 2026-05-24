36f98c50c4b5b8088bb5313560e1bcccc9a0202e
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-05-24 18:58:14 +0300

    refactor: use spotless

--- backend/api-gateway/src/main/java/org/example/ApiGatewayServiceApplication.java
+++ backend/api-gateway/src/main/java/org/example/ApiGatewayServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(ApiGatewayServiceApplication.class);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServiceApplication.class);
+    }
 }
