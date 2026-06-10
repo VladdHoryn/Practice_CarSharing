4c4ff09481a1cef7546d71fa1e827d8c35678661
Oleksandr Grinenko <69903565+Oleksandr1072004@users.noreply.github.com>
2026-06-10 19:02:00 +0300

    Update CarRepository.java

--- backend/car-service/src/main/java/org/example/repository/CarRepository.java
+++ backend/car-service/src/main/java/org/example/repository/CarRepository.java
@@ -1,19 +1,23 @@
 package org.example.repository;
 
-import java.util.List;
-
-import jakarta.validation.constraints.NotNull;
-
 import org.example.domain.Car;
 import org.example.domain.CarStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long>, JpaSpecificationExecutor<Car> {
-    public List<Car> findByStatus(@NotNull(message = "Car status is required") CarStatus status);
 
-    public List<Car> findCarByUserId(
-            @NotNull(message = "Owner does not have any cars") Long userId);
+    List<Car> findByStatus(CarStatus status);
+
+    /**
+     * 4) Кількість авто, які належать певному OWNER
+     */
+    @Query("SELECT COUNT(c) FROM Car c WHERE c.userId = :ownerId")
+    long countByOwnerId(@Param("ownerId") Long ownerId);
 }
